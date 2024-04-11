fa6cb30e7cd57a43587b532538989acd01e0eae6
jerichogatus <157677241+jerichogatus@users.noreply.github.com>
2024-04-11 20:49:52 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div className="container">
+        <h1>Hello,</h1>
+        <div className="next-line">World!</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
